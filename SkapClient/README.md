53ce4d3b202ab72c73033851e68eeb96d84121c6
prookl <85963423+Prookle@users.noreply.github.com>
2024-03-31 17:22:54 -0500

    Add files via upload

---
+++ SkapClient/README.md
@@ -0,0 +1,7 @@
+# SkapClient
+
+[SkapClient](https://nky5223.github.io/SkapClient) is an unofficial client for [skap](https://skap.io).
+
+If you fork SkapClient, please credit me :]
+
+To premiering: no leaking scripts >:(
---
+++ SkapClient/js/Loader.js
@@ -0,0 +1,84 @@
+const Loader = {
+    /** @param {HTMLImageElement} img */
+    image(img) {
+        const promise = new Promise((resolve, reject) => {
+            img.addEventListener("load", _ => {
+                this.loaded++;
+                resolve();
+            });
+            img.addEventListener("error", reject);
+        });
+        
+        promise
+            .then(_ => this.onimgload(img))
+            .catch(err => this.onimgerror(img, err));
+
+        this.promises.push(promise);
+
+        return promise;
+    },
+    /** @param {WebSocket} ws */
+    webSocket(ws) {
+        const promise = new Promise((resolve, reject) => {
+            ws.addEventListener("open", _ => {
+                this.loaded++;
+                resolve();
+            });
+            ws.addEventListener("error", reject);
+        });
+
+        promise
+            .then(_ => this.onwsload(ws))
+            .catch(err => this.onwserror(ws, err));
+
+        this.promises.push(promise);
+
+        return promise;
+    },
+
+
+    /** @type {Promise[]} */
+    promises: [],
+    loaded: 0,
+
+    /**
+     * Replace this with own function
+     * @param {HTMLImageElement} img
+     */
+    onimgload(img) {
+        console.log("Loaded", img.src);
+    },
+    /**
+     * Replace this with own function
+     * @param {HTMLImageElement} img
+     * @param {ErrorEvent} event
+     */
+    onimgerror(img, event) {
+        console.error("Error loading", img.src, event);
+    },
+    /**
+     * Replace this with own function
+     * @param {WebSocket} ws
+     */
+    onwsload(ws) {
+        console.log("Connected to", ws.url);
+    },
+    /**
+     * Replace this with own function
+     * @param {WebSocket} ws
+     * @param {ErrorEvent} event
+     */
+    onwserror(ws, event) {
+        console.error("Error connecting to", ws.url, event);
+    },
+
+    /**
+     * Returns promise that resolves when all things are loaded
+     */
+    start() {
+        const all = Promise.all(this.promises);
+
+        return all;
+    }
+};
+export default Loader;
